69864ac68be3629e767a4e0861ed6d4b14258152
Jeremy Tam Trinh <z5359974@ad.unsw.edu.au>
2021-09-24 00:55:29 +1000

    Added to comments in auth_register_v1 (draft 5)

--- src/auth.py
+++ src/auth.py
@@ -38,7 +38,8 @@ def auth_register_v1(email, password, name_first, name_last):
         raise InputError("Error: email taken")
 
     # Generate handle
-        # If handle is longer than 20 chars, cut off at 20 chars
+        # Concatenate lowercase name_first and name_last
+        # If concatenation is longer than 20 chars, cut off at 20 chars
         # Loop through users looking for repeated handles
         # Increment a repetition counter which will be added to repeated handle
     handle_str = name_first.lower() + name_last.lower()
