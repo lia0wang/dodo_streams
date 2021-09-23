b5db6ccc40e7225a4b5ca3095056d229873f9d83
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-09-23 18:21:39 +1000

    clear_v1

--- src/other.py
+++ src/other.py
@@ -3,4 +3,7 @@ from src.data_store import data_store
 def clear_v1():
     store = data_store.get()
     store['users'] = []
+    store['channels'] = []
+    store['latest_message_id'] = 0
+
     data_store.set(store)
