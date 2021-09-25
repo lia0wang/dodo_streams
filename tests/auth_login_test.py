1361307f3321289d003f052665fff64dae018384
Darren <z5361945@ad.unsw.edu.au>
2021-09-25 15:22:45 +1000

    Minor change added case where email also has numbers

--- tests/auth_login_test.py
+++ tests/auth_login_test.py
@@ -14,11 +14,13 @@ def test_valid_login():
     auth_register_v1("JoJo@gmail.com", "HermitPurple", "Joseph", "Joestar")
     auth_register_v1("testA@gmail.com", "testA2021", "John", "smith")
     auth_register_v1("StandUser@gmail.com", "StandPower", "Generic", "name")
+    auth_register_v1("11037.666@gmail.com", "armStrongCann0n", "Isaac", "Schneider")
 
     assert auth_login_v1("StandUser@gmail.com", "StandPower") == 3
     assert auth_login_v1("JoJo@gmail.com", "HermitPurple") == 1
     assert auth_login_v1("testA@gmail.com", "testA2021") == 2
-
+    assert auth_login_v1("11037.666@gmail.com", "armStrongCann0n") == 4
+    
 def test_invalid_email():
 
     clear_v1()
@@ -26,6 +28,8 @@ def test_invalid_email():
     auth_register_v1("JoJo@gmail.com", "HermitPurple", "Joseph", "Joestar")
     with pytest.raises(InputError):
         auth_login_v1("jojo@gmail.com", "HermitPurple")
+    with pytest.raises(InputError):
+        auth_login_v1("jojo@gmail.com", "KermittheHermit")
     with pytest.raises(InputError):
         auth_login_v1("jojossds@gmail.com", "HermitPurple")
 
