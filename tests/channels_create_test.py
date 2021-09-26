20d93890b2998604095ef86a3084dadfd6080cc6
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-09-26 19:46:10 +1000

    changed variables name

--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -4,7 +4,7 @@ from src.auth import auth_register_v1
 from src.channels import channels_create_v1
 from src.error import InputError
 
-def test_channel_is_dict():
+def test_channel_type():
     clear_v1()
     u_id = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
     channel_id = channels_create_v1(u_id, 'league', True)
@@ -12,7 +12,7 @@ def test_channel_is_dict():
     # isinstance returns true when the type of 2 parameters are equal.
     assert(isinstance(channel_id, dict) == True) 
 
-def test_multiple_channels_id():
+def test_multiple_channels():
     clear_v1()
     u_id1 = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
     u_id2 = auth_register_v1('liaowang@gmail.com', 'liaowang0207', 'wang', 'liao')
@@ -21,7 +21,7 @@ def test_multiple_channels_id():
     
     assert(channel_id_1['channel_id'] == 1)
     assert(channel_id_2['channel_id'] == 2)
-
+    
 def test_no_channel_name():
     clear_v1()
     u_id = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
