2e4355ce919ca3aa20bfbed34150e74dd08a0e16
Ishan Upadhyay <z5361954@ad.unsw.edu.au>
2021-09-27 18:52:22 +1000

    Completed testing files

--- tests/channel_list_v1_test.py
+++ tests/channel_list_v1_test.py
@@ -10,28 +10,58 @@ from src.error import InputError
 # Entered auth_u_id is incorrect
 def test_nonexistent_auth_uid():
     clear_v1()
-    channels_list_v1()
+    channels_list_v1(35) == {}
 
 # No channels are created
 def test_no_channels():
     clear_v1()
+    auth_user_id = auth_register_v1("bob123@gmail.com", "qwerty", "Bob", "Marley")
+    # Need to check this
+    assert channels_list_v1(auth_user_id) == {}
 
 
 # No channels the auth_u_id is part of
 def test_no_channels_for_user():
     clear_v1()
+    auth_user_id0 = auth_register_v1("bob123@gmail.com", "qwerty", "Bob", "Marley")
+    auth_user_id1 = auth_register_v1("bill123@gmail.com", "asdfgh", "Bill", "Gates")
+    channels_create_v1(auth_user_id1, False)
+    assert channels_list_v1(auth_user_id0) == {}
+
 
 
 # No members except auth_u_id in channels
 def test_no_members_of_channel():
     clear_v1()
+    auth_user_id0 = auth_register_v1("bob123@gmail.com", "qwerty", "Bob", "Marley")
+    channel_id = channels_create_v1(auth_user_id0, "test_channel", False)
+    assert channels_list_v1(auth_user_id0) == {'channels': [{'channel_id': channel_id, 'name': "test_channel"}]}
 
 
 # Public channel with many users
 def test_many_users_in_channel():
     clear_v1()
+    auth_user_id0 = auth_register_v1("bob123@gmail.com", "qwerty", "Bob", "Marley")
+    channel_id = channels_create_v1(auth_user_id0, "test_channel", True)
+    auth_user_id1 = auth_register_v1("bill123@gmail.com", "asdfgh", "Bill", "Gates")
+    auth_user_id2 = auth_register_v1("tardis@gmail.com", "thedoctor", "John", "Smith")
+    auth_user_id3 = auth_register_v1("sauron@gmail.com", "theshire", "Frodo", "Baggins")
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id1)
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id2)
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id3)
+    assert channels_list_v1(auth_u_id0) == {'channels': [{'channel_id': channel_id, 'name': "test_channel"}]}
+
 
 
 # Private channel with auth_u_id
 def test_private_channels():
     clear_v1()
+    auth_user_id0 = auth_register_v1("bob123@gmail.com", "qwerty", "Bob", "Marley")
+    channel_id = channels_create_v1(auth_user_id0, "test_channel", False)
+    auth_user_id1 = auth_register_v1("bill123@gmail.com", "asdfgh", "Bill", "Gates")
+    auth_user_id2 = auth_register_v1("tardis@gmail.com", "thedoctor", "John", "Smith")
+    auth_user_id3 = auth_register_v1("sauron@gmail.com", "theshire", "Frodo", "Baggins")
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id1)
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id2)
+    channel_invite_v1(auth_u_id0, channel_id, auth_user_id3)
+    assert channels_list_v1(auth_u_id0) == {'channels': [{'channel_id': channel_id, 'name': "test_channel"}]}
