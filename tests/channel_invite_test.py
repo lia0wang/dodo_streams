db524f26517f82d10a7c19d29c0cb9e72a04143d
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-09-29 18:01:44 +1000

    first draft of channel_invite_test.py

---
+++ tests/channel_invite_test.py
@@ -0,0 +1,76 @@
+import pytest
+from src.auth import auth_register_v1
+from src.channel import channel_join_v1
+from src.channel import channel_invite_v1
+from src.channels import channels_create_v1
+from src.channels import channels_listall_v1
+from src.error import InputError, AccessError
+from src.data_store import data_store
+
+def test_valid_input():
+    '''tests for valid channel_invite_v1 implementation'''
+    clear_v1()
+    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
+    auth_id1 = user1['auth_user_id']
+    auth_id2 = user2['auth_user_id']
+    channel = channels_create_v1(auth_id1, 'comp1531', True)
+    channel_id = channel["channel_id"]
+    channel_invite_v1(auth_id1,channel_id,auth_id2)
+
+    channel_test_id = channels_listall_v1(auth_id2)
+    output = channel_test_id['channels'][0]['channel_id']
+    
+    assert output == 1
+
+
+'''Input Error Testing Module'''
+
+def test_invalid_channel():
+    '''tests for valid channel_invite_v1 with invalid channel_id'''
+    clear_v1()
+    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
+    auth_id1 = user1['auth_user_id']
+    auth_id2 = user2['auth_user_id']
+    channel = channels_create_v1(auth_id1, 'comp1531', True)
+    channel_id = channel["channel_id"]
+    channel_invite_v1(auth_id1,channel_id,auth_id2)
+    invalid_channel = 2
+
+    
+    with pytest.raises(InputError):
+        channel_invite_v2(auth_id1, invalid_channel, auth_id2)
+
+def test_invalid_user():
+    '''tests for valid channel_invite_v1 with invalid auth_user_id'''
+    clear_v1()
+    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    
+    auth_id1 = user1['auth_user_id']
+    invalid_u_id = 777
+    channel = channels_create_v1(auth_id1, 'comp1531', True)
+    channel_id = channel["channel_id"]
+    channel_invite_v1(auth_id1,channel_id,invalid_u_id)
+    channel_id = channel["channel_id"]
+    
+    with pytest.raises(InputError):
+        channel_invite_v2(auth_id1, channel_id, invalid_u_id)
+
+def test_repeated_user():
+    '''tests for valid channel_invite_v1 with repeated auth_user_id'''
+    clear_v1()
+    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    auth_id1 = user1['auth_user_id']
+    channel = channels_create_v1(auth_id1, 'comp1531', True)
+    channel_id = channel["channel_id"]
+    channel_invite_v1(auth_id1,channel_id,auth_id1)
+    channel_id = channel["channel_id"]
+    output = channel_test_id['channels'][0]['channel_id']
+    with pytest.raises(InputError):
+        channel_invite_v2(auth_id1, channel_id, auth_id1)
+
+'''Access Error Testing Module'''
+
+
+        
