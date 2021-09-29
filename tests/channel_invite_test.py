c878a84d8dd124a086b8f0bbaf8740cc35e0ce77
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-09-29 18:17:05 +1000

    first draft of channel_invite_test.py

--- tests/channel_invite_test.py
+++ tests/channel_invite_test.py
@@ -40,7 +40,7 @@ def test_invalid_channel():
 
     
     with pytest.raises(InputError):
-        channel_invite_v2(auth_id1, invalid_channel, auth_id2)
+        channel_invite_v1(auth_id1, invalid_channel, auth_id2)
 
 def test_invalid_user():
     '''tests for valid channel_invite_v1 with invalid auth_user_id'''
@@ -55,22 +55,28 @@ def test_invalid_user():
     channel_id = channel["channel_id"]
     
     with pytest.raises(InputError):
-        channel_invite_v2(auth_id1, channel_id, invalid_u_id)
+        channel_invite_v1(auth_id1, channel_id, invalid_u_id)
 
-def test_repeated_user():
-    '''tests for valid channel_invite_v1 with repeated auth_user_id'''
+def test_assigned_user():
+    '''tests for valid channel_invite_v1 with a user is already a member of the channel'''
     clear_v1()
     user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
     auth_id1 = user1['auth_user_id']
+    auth_id2 = user2['auth_user_id']
     channel = channels_create_v1(auth_id1, 'comp1531', True)
     channel_id = channel["channel_id"]
-    channel_invite_v1(auth_id1,channel_id,auth_id1)
-    channel_id = channel["channel_id"]
-    output = channel_test_id['channels'][0]['channel_id']
+    channel_invite_v1(auth_id1,channel_id,auth_id2)
+
+    channel_invite_v1(auth_id1,channel_id,auth_id2)
     with pytest.raises(InputError):
-        channel_invite_v2(auth_id1, channel_id, auth_id1)
+        channel_invite_v1(auth_id1, channel_id, auth_id12)
 
 '''Access Error Testing Module'''
 
+def test_noaccess():
+'''tests for whannel_id is valid and the authorised user is not a member of the channel'''
+    pass
+    
 
         
