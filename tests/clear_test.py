dcf07de84131ef150bc85fd3077f4e145cedd751
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-10-02 14:51:59 +1000

    Add tests for clear_v1

---
+++ tests/clear_test.py
@@ -0,0 +1,42 @@
+import pytest
+from src.other import clear_v1
+from src.auth import auth_register_v1, auth_login_v1
+from src.channel import channel_join_v1
+from src.channels import channels_listall_v1, channels_create_v1
+from src.error import InputError, AccessError
+
+def test_return_type():
+    """ 
+    Test if the ruturn type for clear_v1 is {}
+    """
+    assert clear_v1() == {}
+
+def test_clear_user():
+    """ 
+    Test if clear_v1 successfully clear the user
+    """
+    clear_v1()
+    user = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
+    clear_v1()
+
+    with pytest.raises(InputError):
+        auth_login_v1('wangliao@gmail.com', 'liaowang0207')
+
+def test_clear_channel():
+    """ 
+    Test if clear_v1 successfully clear the channel
+    """
+    clear_v1()
+    user_1 = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
+    channel_1 = channels_create_v1(user_1['auth_user_id'], 'league', True)
+    user_2 = auth_register_v1('shifan@gmail.com', 'shifan0207', 'shifan', 'chen')
+    channel_2 = channels_create_v1(user_2['auth_user_id'], 'legend', True)
+    
+    channel_join_v1(user_2['auth_user_id'], channel_1['channel_id'])
+    channel_join_v1(user_1['auth_user_id'], channel_2['channel_id'])
+    
+    clear_v1()
+        
+    with pytest.raises(AccessError):
+        channels_listall_v1(user_1['auth_user_id'])
+        channels_listall_v1(user_2['auth_user_id'])
