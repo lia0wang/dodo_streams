2f1cd912a2cf6324e2a390221c81b201f4b2a73a
Jeremy Tam Trinh <z5359974@ad.unsw.edu.au>
2021-10-15 22:38:49 +1100

    made fixes to functions for saving database

--- src/helper.py
+++ src/helper.py
@@ -37,35 +37,65 @@ def create_session_id():
     creates a new session ID
 
     Returns:
-        SESSION_ID (int)
+        - SESSION_ID (int)
     """
     global SESSION_ID 
     SESSION_ID  += 1
     return SESSION_ID
 
-def save_data():
+# Only saves updates to users and channels
+# Might need to be updated later
+def save_data_store_updates():
     '''
-    Saves data to json file database
+    Saves updates to data_store. 
+    Saves it to json file database
     '''
     store = data_store.get()
     filesize = os.path.getsize("database.json")
-    # Store data_store if json file is empty
+    # Store data_store data if json file is empty
     if filesize == 0:
         with open('database.json', 'w') as file:
             json.dump(store, file)
         file.close()
-    # Append new data from data_store if not empty
+    # Add new data from data_store if not empty
     else:
         with open("database.json") as file:
             data = json.load(file)
         file.close()
+        # Update 'already existing' users information 
+        for updated_user in store['users']:
+            for index, user in enumerate(data['users']):
+                if user['u_id'] == updated_user['u_id'] and user != updated_user:
+                    data['users'][index] = updated_user
+        # Append new users
         for user in store['users']:
-            data['users'].append(user)
+            if user not in data['users']:
+                data['users'].append(user)
+
+        # Update 'already existing' channels information
+        for updated_chann in store['channels']:
+            for index, chann in enumerate(data['channels']):
+                if chann['channel_id'] == updated_chann['channel_id'] and chann != updated_chann:
+                    data['channels'][index] = updated_chann
+        # Append new channels
         for channel in store['channels']:
-            data['channels'].append(channel)
+            if channel not in data['channels']:
+                data['channels'].append(channel)
+
         with open('database.json', 'w') as file:
             json.dump(data, file)
         file.close()
+
+def save_database_updates(updated_database):
+    '''
+    Saves updates to database.
+    Saves it to json database.
+    Arguments:
+        updated_database (dictionary)
+    '''
+    with open('database.json', 'w') as file:
+        json.dump(updated_database, file)
+    file.close()
 
 def get_data():
     '''
@@ -73,6 +103,9 @@ def get_data():
     Returns:
         dictionary in a smilar manner to datastore.get
     '''
+    filesize = os.path.getsize("database.json")
+    if filesize == 0:
+        return {}
     with open('database.json') as file:
         data = json.load(file)
     file.close()
