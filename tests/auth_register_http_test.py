f73fd36e749582d1ac2270c0be8c016d5c433304
Jeremy Tam Trinh <z5359974@ad.unsw.edu.au>
2021-10-16 19:37:57 +1100

    added status code testsing

--- tests/auth_register_http_test.py
+++ tests/auth_register_http_test.py
@@ -15,8 +15,21 @@ def test_http_register_basic():
     }
     get_response =  requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
     open('database.json', 'w').close()
-    assert get_response == 200
+    assert get_response.status_code == 200
     
+
+def test_http_name_first_short():
+    open('database.json', 'w').close()
+    register_param = {
+        "email": "11037@gmail.com",
+        "password": "Hope11037",
+        "name_first": "",
+        "name_last": "Boyyy"
+    }
+    get_response = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
+    open('database.json', 'w').close()
+    assert get_response.status_code != 200
+
 
 
 
