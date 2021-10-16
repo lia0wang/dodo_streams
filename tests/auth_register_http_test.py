1de9e0e46943314671dc4bb272b9a2d4faa61bbb
Jeremy Tam Trinh <z5359974@ad.unsw.edu.au>
2021-10-16 19:29:57 +1100

    editing testing errors

--- tests/auth_register_http_test.py
+++ tests/auth_register_http_test.py
@@ -5,89 +5,17 @@ from src.error import AccessError, InputError
 
 BASE_URL = 'http://localhost:8080'
 
-
-def test_http_invalid_email():
+def test_http_register_basic():
     open('database.json', 'w').close()
     register_param = {
-        "email": "11037.666@gmail&.com",
+        "email": "11037.666@gmail.com",
         "password": "Hope11037",
         "name_first": "Hopeful",
         "name_last": "Boyyy"
     }
-    with pytest.raises(InputError):
-         requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-    register_param['email'] = "@xample.com"
-    with pytest.raises(InputError):
-         requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-def test_http_duplicate_email():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "Hope11037",
-        "name_first": "Hopeful",
-        "name_last": "Boyyy"
-    }
-    requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-
-def test_http_password_short():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "12345",
-        "name_first": "Hopeful",
-        "name_last": "Boyyy"
-    }
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-def test_http_name_first_long():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "Hope11037",
-        "name_first": "MynameisYoshikageKiraIm33yearsoldMyhouseisinthenort",
-        "name_last": "Boyyy"
-    }
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-def test_http_name_last_long():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "Hope11037",
-        "name_first": "Mynameis",
-        "name_last": "MynameisYoshikageKiraIm33yearsoldMyhouseisinthenort"
-    }
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-
-def test_http_name_first_short():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "Hope11037",
-        "name_first": "",
-        "name_last": "Boyyy"
-    }
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
-
-def test_http_name_last_short():
-    open('database.json', 'w').close()
-    register_param = {
-        "email": "11037@gmail.com",
-        "password": "Hope11037",
-        "name_first": "Mynameis",
-        "name_last": ""
-    }
-    with pytest.raises(InputError):
-        requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
+    get_response =  requests.post(f"{BASE_URL}/auth/register/v2", json = register_param)
+    assert get_response == 200
+    
 
 
 
