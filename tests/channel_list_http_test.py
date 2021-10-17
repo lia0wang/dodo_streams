1130fb02822515c883c16561d00600a88fb3c32c
Ishan Upadhyay <z5361954@ad.unsw.edu.au>
2021-10-17 19:34:55 +1100

    Added basic tests

---
+++ tests/channel_list_http_test.py
@@ -0,0 +1,33 @@
+import pytest
+import requests
+from json import loads
+from src.other import clear_v1
+
+BASE_URL = 'http://localhost:8080'
+
+
+def test_http_nonexistent_email():
+    clear_v1()
+    get_response = requests.get(f"{BASE_URL}/channels/list/v2", json = 389)
+    assert get_response.status_code != 200
+
+def test_http_valid_result():
+    clear_v1()
+    param0 = {
+        "email": "bob123@gmail.com",
+        "password": "qwerty",
+        "name_first": "Bob",
+        "name_last": "Marley"
+    }
+    user = requests.post(f"{BASE_URL}/auth/register/v2", json = param0)
+    user0 = loads(user.json())
+
+    channel_param0 = {
+        "token": user0['token'],
+        "name": "test_channel1",
+        "is_public": True
+    }
+    requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param0)
+
+    get_response = requests.get(f"{BASE_URL}/channels/list/v2", json = user0['token'])
+    assert get_response.status_code == 200
