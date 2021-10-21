7ab06ece16f0fdb14dc4f75e28db3bc1574272e7
Ishan Upadhyay <z5361954@ad.unsw.edu.au>
2021-10-21 15:27:30 +1100

    Changed tests to reduce mistakes and match create channel function

--- tests/channel_list_http_test.py
+++ tests/channel_list_http_test.py
@@ -6,28 +6,104 @@ from src.other import clear_v1
 BASE_URL = 'http://localhost:8080'
 
 
-def test_http_nonexistent_token():
-    clear_v1()
-    get_response = requests.get(f"{BASE_URL}/channels/list/v2", json = 389)
-    assert get_response.status_code != 200
-
-def test_http_valid_result():
-    clear_v1()
-    param0 = {
+def test_http_list_basic():
+    '''
+    Testing whether the v2 function works properly
+    '''
+    requests.delete(f"{BASE_URL}/clear/v1", json={})
+    
+    register_param = {
         "email": "11037.666@gmail.com",
         "password": "Hope11037",
         "name_first": "Hopeful",
         "name_last": "Boyyy"
     }
-    user = requests.post(f"{BASE_URL}/auth/register/v2", json = param0).json()
-    # user0 = loads(user)
+    user = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param).json()
 
-    channel_param0 = {
-        "token": user['token'],
-        "name": "test_channel1",
-        "is_public": True
+    channel_param = {
+        'token': user['token'],
+        'name': 'league',
+        'is_public': True
     }
-    requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param0)
+    channel = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
+    
+    token = {
+        'token': user['token']
+    }
+    
+    response = requests.get(f"{BASE_URL}/channels/list/v2", json = token)
+    
+    assert response.status_code == 200
+    channel_list = response.json()
+    
+    assert channel_list == {'channels': [{'channel_id': channel['channel_id'], 'name': 'league'}]}
 
-    get_response = requests.get(f"{BASE_URL}/channels/list/v2", json = user['token'])
-    assert get_response.status_code == 200
+def test_http_invalid_token():
+    '''
+    Testing whether the v2 function can identify incorrect tokens
+    '''
+    requests.delete(f"{BASE_URL}/clear/v1", json={})
+    
+    register_param = {
+        "email": "11037.666@gmail.com",
+        "password": "Hope11037",
+        "name_first": "Hopeful",
+        "name_last": "Boyyy"
+    }
+    user = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param).json()
+
+    channel_param = {
+        'token': user['token'],
+        'name': 'league',
+        'is_public': True
+    }
+    requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
+    
+    token = {
+        'token': user['token'] + '1' # Incorrect token
+    }
+    
+    response = requests.get(f"{BASE_URL}/channels/list/v2", json = token)
+    
+    assert response.status_code != 200
+
+def test_http_list_multiple():
+    '''
+    Test when create multiple channels
+    '''
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param = {
+        "email": "11037.666@gmail.com",
+        "password": "Hope11037",
+        "name_first": "Hopeful",
+        "name_last": "Boyyy"
+    }
+    user = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param).json()
+
+    channel_param_1 = {
+        'token': user['token'], # token invalid
+        'name': 'league1',
+        'is_public': True
+    }
+
+    channel_param_2 = {
+        'token': user['token'], # token invalid
+        'name': 'league2',
+        'is_public': False
+    }
+
+    channel_1 = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param_1).json()
+    channel_2 = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param_2).json()
+    
+    token = {
+        'token': user['token']
+    }
+    
+    response = requests.get(f"{BASE_URL}/channels/list/v2", json = token)
+    
+    assert response.status_code == 200
+    channel_list = response.json()
+    
+    assert channel_list == {'channels': [{'channel_id': channel_1['channel_id'], 'name': 'league1'}, 
+                                         {'channel_id': channel_2['channel_id'], 'name': 'league2'}]}
