900fc8ca32b049d4dcbed90357bcf38d619a883a
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-10-21 15:01:02 +1100

    A rough draft of message/send/v1 and senddm/v1, with data_store.py

--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 from src.channel import channel_join_v1, channel_details_v1
 from src.channels import channels_create_v1
 from src.dm import dm_create_v1
-from src.message import send_v1
+from src.message import send_v1, senddm_v1
 from src.error import InputError
 from src import config
 from src.auth import auth_register_v1, auth_login_v1
@@ -200,6 +200,22 @@ def message_send():
     new_message = send_v1(token,channel_id,message)
     save_database_updates(new_message)
     return dumps(new_message)
+
+@APP.route("/message/senddm/v1", methods=['POST'])
+def message_senddm():
+    request_data = request.get_json()
+    # Retrieve token
+    token = request_data['token']
+    check_valid_token(token)
+
+    # Retrieve channel id
+    dm_id = request_data['dm_id']
+    # Retrieve message
+    message = request_data['message']
+    # Pass parameters
+    new_dm = send_v1(token,dm_id,message)
+    save_database_updates(new_dm)
+    return dumps(new_dm)
 
 @APP.route("/user/profile/v1", methods=['GET'])
 def profile():
