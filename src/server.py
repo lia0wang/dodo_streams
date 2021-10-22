8de3132b2ab5597fb6f9d93167bde7e640c89b73
Darren <z5361945@ad.unsw.edu.au>
2021-10-22 23:51:31 +1100

    Merged master into channel_messages branch

--- src/server.py
+++ src/server.py
@@ -4,8 +4,7 @@ import re
 from json import dump, dumps
 from flask import Flask, request
 from flask_cors import CORS
-from src.channel import channel_addowner_v1, channel_join_v1, \
-channel_details_v1, channel_leave_v1, channel_removeowner_v1,channel_messages_v1
+from src.channel import channel_addowner_v1, channel_join_v1, channel_details_v1, channel_removeowner_v1, channel_messages_v1
 from src.channels import channels_create_v1
 from src.dm import dm_create_v1, dm_details_v1
 from src.message import message_send_v1, message_senddm_v1
@@ -14,8 +13,8 @@ from src import config
 from src.auth import auth_register_v1, auth_login_v1
 from src.channels import channels_list_v1, channels_listall_v1
 from src.data_store import data_store
-from src.helper import check_valid_token, get_data, save_data_store_updates,\
-create_session_id, save_database_updates, create_jwt, decode_jwt
+from src.helper import check_valid_token, get_data, save_data_store_updates, create_session_id
+from src.helper import is_database_exist, save_database_updates, create_jwt, decode_jwt, hash_encrypt
 from src.other import clear_v1
 
 def quit_gracefully(*args):
@@ -171,6 +170,7 @@ def logout():
             save_database_updates(db_store)
     return dumps({})
 
+
 @APP.route("/channel/join/v2", methods=['POST'])
 def channel_join():
     # Retrieve token
@@ -184,23 +184,6 @@ def channel_join():
 
     # Pass parameters
     channel_join_v1(decode_token['u_id'], channel_id)
-    save_data_store_updates()
-    
-    return dumps({})
-
-@APP.route("/channel/leave/v2", methods=['POST'])
-def channel_leave():
-    # Retrieve token
-    request_data = request.get_json()
-    token = request_data['token']
-    check_valid_token(token)
-
-    # Decode token, retrieve parameters
-    decode_token = decode_jwt(token)
-    channel_id = request_data['channel_id']
-
-    # Pass parameters
-    channel_leave_v1(decode_token['u_id'], channel_id)
     save_data_store_updates()
     
     return dumps({})
@@ -310,28 +293,17 @@ def dm_leave():
     return dumps({}) 
 @APP.route("/dm/list/v1", methods=['GET'])
 def dm_list():
-    # retrieve token
-    data = request.get_json()
-    token = data['token']
     
-    # Checking and decoding token
-    check_valid_token(token)
-    decoded_jwt = decode_jwt(token)
-    u_id = decoded_jwt['u_id']
-    
-    # Getting dm data and making dms list
+    # Getting dm list
     store = get_data()
     dms = []
-    
-    # Traversing through dms, appending those that
-    # have u_id as a member
     for dm in store['dms']:
-        if u_id in dm['u_ids']:
-            new_dm = dm
-            del new_dm['auth_user_id']
-            del new_dm['u_ids']
-            del new_dm['messages']
-            dms.append(new_dm)
+        new_dm = dm
+        del new_dm['auth_user_id']
+        del new_dm['u_ids']
+        del new_dm['messages']
+        dms.append(new_dm)
+    
     return dumps(dms)
 
 @APP.route("/dm/details/v1", methods=['GET'])
