9284b9cbd6c272da58b4be03fcba29131f0cf701
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-10-23 11:57:31 +1100

    Updated the tests for channel/invite/v2

--- tests/channel_invite_test.py
+++ tests/channel_invite_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.other import clear_v1
 from src.auth import auth_register_v1
-from src.channel import channel_join_v1
+from src.channel import channel_join_v1, channel_leave_v1
 from src.channel import channel_invite_v1
 from src.channels import channels_create_v1
 from src.channels import channels_listall_v1
@@ -36,7 +36,7 @@ def test_invalid_channel():
     channel = channels_create_v1(auth_id, 'comp1531', True)
     channel_id = channel["channel_id"]
     channel_invite_v1(auth_id,channel_id,u_id)
-    invalid_channel1 = channel_id + 1
+    invalid_channel1 = channel_id + 999
     invalid_channel2 = -1
     
     with pytest.raises(InputError):
@@ -51,12 +51,12 @@ def test_invalid_user():
     user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
     auth_id = user1['auth_user_id']
     u_id = user2['auth_user_id']
-    invalid_auth_id = auth_id - 1
-    invalid_u_id = u_id + 1
+    invalid_auth_id = auth_id + 999
+    invalid_u_id = u_id + 888
     channel = channels_create_v1(auth_id, 'comp1531', True)
     channel_id = channel["channel_id"]
     
-    with pytest.raises(InputError):
+    with pytest.raises(AccessError):
         channel_invite_v1(invalid_auth_id, channel_id, u_id)
 
     with pytest.raises(InputError):
@@ -78,23 +78,19 @@ def test_repeated_user():
     with pytest.raises(InputError):
         channel_invite_v1(auth_id, channel_id, auth_id)
 
-
-
-'''Access Error Testing Module'''
-
 def test_unauthorised_user():
-    '''tests for channel_id is valid and the authorised user is not a member of the channel'''
+    '''
+    Tests for channel_id is valid and the authorised user
+    is not a member of the channel
+    '''
     clear_v1()
-    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
-    user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
-    user3 = auth_register_v1("AgentJohnson@hotmail.com", "abcd1234", "Agent", "Johnson")
-    auth_id = user1['auth_user_id']
-    u_id = user2['auth_user_id']
-    u_id2 = user3['auth_user_id']
-    channel = channels_create_v1(auth_id, 'comp1531', True)
-    channel_id = channel["channel_id"]
+    owner = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
+    user1 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
+    user2 = auth_register_v1("AgentJohnson@hotmail.com", "abcd1234", "Agent", "Johnson")
+
+    channel = channels_create_v1(owner['auth_user_id'], 'comp1531', True)
+    channel_invite_v1(owner['auth_user_id'], channel["channel_id"], user1['auth_user_id'])
+    channel_leave_v1(owner['auth_user_id'], channel["channel_id"])
 
     with pytest.raises(AccessError):
-        channel_invite_v1(u_id, channel_id, u_id2)
-    
-
+        channel_invite_v1(owner['auth_user_id'], channel["channel_id"], user2['auth_user_id'])
