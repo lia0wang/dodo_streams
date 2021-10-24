b31fd8a7b544015271c907069fc3ddfb66777ae0
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-10-24 15:09:26 +1100

    messgae/edit/v1 & message/remove/v1

--- tests/message_senddm_test.py
+++ tests/message_senddm_test.py
@@ -9,11 +9,7 @@ from src.error import InputError, AccessError
 
 ### Input Error
 
-<<<<<<< Updated upstream:tests/message_test.py
-def test_invalid_input_length():
-=======
 def test_dm_invalid_input_length():
->>>>>>> Stashed changes:tests/message_senddm_test.py
     '''tests for invalid message length'''
     clear_v1()
     session_id = create_session_id()
@@ -38,11 +34,9 @@ def test_dm_invalid_input_length():
     with pytest.raises(InputError):
         message_senddm_v1(token, dm_id, invalid_msg_2)
 
-<<<<<<< Updated upstream:tests/message_test.py
-def test_invalid_input_length_dm():
-=======
+
+
 def test_dm_invalid_dm_id():
->>>>>>> Stashed changes:tests/message_senddm_test.py
     '''tests for invalid message length'''
     clear_v1()
     session_id = create_session_id()
@@ -69,30 +63,9 @@ def test_dm_invalid_dm_id():
         message_senddm_v1(token, invalid_dm_id_2, msg)
 
 ### Access Error
-<<<<<<< Updated upstream:tests/message_test.py
-def test_invalid_msg_auth_id():
-    clear_v1()
-    session_id = create_session_id()
-    user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
-    user2 = auth_register_v1("JohnSmith@hotmail.com", "abcd1234", "John", "Smith")
-    
-    auth_id = user1['auth_user_id']
-    channel = channels_create_v1(auth_id, 'comp1531', True)
-    channel_id = channel["channel_id"]
-
-    u_id = user2['auth_user_id']
-    invalid_token = create_jwt(u_id, session_id)
-    
-    msg = "Something"
-    
-    with pytest.raises(AccessError):
-        message_send_v1(invalid_token, channel_id, msg)
-
-def test_invalid_msg_auth_id_dm():
-=======
 
 def test_dm_invalid_auth_id():
->>>>>>> Stashed changes:tests/message_senddm_test.py
+
     clear_v1()
     session_id = create_session_id()
     user1 = auth_register_v1("AgentSmith@hotmail.com", "abcd1234", "Agent", "Smith")
@@ -111,9 +84,5 @@ def test_dm_invalid_auth_id():
     
     msg = "Something"
     with pytest.raises(AccessError):
-<<<<<<< Updated upstream:tests/message_test.py
-        message_senddm_v1(invalid_token, dm_id['dm_id'], msg)
-    
-=======
         message_senddm_v1(invalid_token, dm_id, msg)
->>>>>>> Stashed changes:tests/message_senddm_test.py
+
