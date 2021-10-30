19e549912c4eb5c2921d7dfea6fb85ffbb69a6b3
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-10-30 23:57:01 +1100

    Fix bugs in channel/removeowner/v1

--- tests/channel_removeowner_test.py
+++ tests/channel_removeowner_test.py
@@ -5,6 +5,55 @@ from src.channel import channel_join_v1, channel_details_v1, channel_addowner_v1
 from src.channels import channels_create_v1
 from src.error import InputError, AccessError
 
+def test_global_owner_nonmember_cannot_remove_owner():
+    '''
+    Test when the authorized user is a global owner but not a member of the channel
+    '''
+    clear_v1()
+    global_owner = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
+    user = auth_register_v1('chenshifan@gmail.com', 'chenshifan0207', 'shifan', 'chen')
+
+    channel = channels_create_v1(user['auth_user_id'], 'league', True)
+
+    assert global_owner['auth_user_id'] not in [user['u_id'] for user in channel_details_v1(user['auth_user_id'], channel['channel_id'])['owner_members']]
+    assert global_owner['auth_user_id'] not in [user['u_id'] for user in channel_details_v1(user['auth_user_id'], channel['channel_id'])['all_members']]
+
+    with pytest.raises(AccessError):
+        channel_removeowner_v1(global_owner['auth_user_id'], channel['channel_id'], user['auth_user_id'])
+
+def  test_nonmember_cannot_remove_owner():
+    '''
+    Test when the authorized user is not a member of the channel
+    '''
+    clear_v1()
+    owner = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
+    channel = channels_create_v1(owner['auth_user_id'], 'league', True)
+
+    user = auth_register_v1('chenshifan@gmail.com', 'chenshifan0207', 'shifan', 'chen')
+
+    assert user['auth_user_id'] not in [user['u_id'] for user in channel_details_v1(owner['auth_user_id'], channel['channel_id'])['owner_members']]
+    assert user['auth_user_id'] not in [user['u_id'] for user in channel_details_v1(owner['auth_user_id'], channel['channel_id'])['all_members']]
+
+    with pytest.raises(AccessError):
+        channel_removeowner_v1(user['auth_user_id'], channel['channel_id'], owner['auth_user_id'])
+
+def test_member_cannot_remove_owner():
+    '''
+    Test when the authorized user is a member not a owner
+    '''
+    clear_v1()
+    owner = auth_register_v1('wangliao@gmail.com', 'liaowang0207', 'wang', 'liao')
+    channel = channels_create_v1(owner['auth_user_id'], 'league', True)
+
+    user = auth_register_v1('chenshifan@gmail.com', 'chenshifan0207', 'shifan', 'chen')
+    channel_join_v1(user['auth_user_id'], channel['channel_id'])
+
+    assert user['auth_user_id'] not in [user['u_id'] for user in channel_details_v1(owner['auth_user_id'], channel['channel_id'])['owner_members']]
+    assert user['auth_user_id'] in [user['u_id'] for user in channel_details_v1(owner['auth_user_id'], channel['channel_id'])['all_members']]
+
+    with pytest.raises(AccessError):
+        channel_removeowner_v1(user['auth_user_id'], channel['channel_id'], owner['auth_user_id'])
+
 def test_invalid_channel_id():
     '''
     Test when the channel_id is invalid
