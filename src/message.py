d631b4d83257a7cb81f2b4879687cd6c6f8c2f69
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-11-03 22:19:13 +1100

    fixed bugs in dm/remove and message/edit, message/remove

--- src/message.py
+++ src/message.py
@@ -1,6 +1,6 @@
 import re
 import os
-from src.dm import dm_create_v1
+from src.dm import dm_create_v1, dm_details_v1
 from src.helper import get_data, check_valid_token,\
      decode_jwt,save_database_updates, datetime_to_unix_time_stamp
 from src.channel import channel_details_v1, channel_messages_v1
@@ -73,12 +73,8 @@ def message_send_v1(token, channel_id, message):
         'time_created': timestamp
     }
 
-    target_channel['messages'].append(message)
-    db_store['messages'].append(message)
-    
-    
+    target_channel['messages'].append(message)  
     save_database_updates(db_store)
-
 
     return {
         'message_id': message_id,
@@ -106,14 +102,14 @@ def message_edit_v1(token, message_id, new_message):
     # Fetch data
     auth_request = False
     has_owner_permission = False
-    valid_message = False
+    valid_channel_message = False
+    valid_dm = False
     if len(new_message)>1000:
         raise InputError("Error: message too long")
     
     elif len(new_message)<1:
         message_remove_v1(token, message_id)
         return None
-    
 
     db_store = get_data()
         
@@ -129,7 +125,7 @@ def message_edit_v1(token, message_id, new_message):
     for channel in db_store['channels']:
         for message in channel['messages']:
             if message['message_id'] == message_id:
-                valid_message = True
+                valid_channel_message = True
                 if message['u_id'] == auth_user_id:
                     auth_request = True
                 for users in channel['owner_members']:
@@ -141,17 +137,41 @@ def message_edit_v1(token, message_id, new_message):
                     raise AccessError("Authorised user does not have owner permisson \
                           of the channel or the message was not sent by the \
                           authorised user making this request")
-    if valid_message and auth_request:
+    if valid_channel_message and auth_request:
         for channel in db_store['channels']:
             for msg in channel['messages']:
                 if msg['message_id'] == message_id:
                     msg['message'] = new_message
                     save_database_updates(db_store)
-                    
-    if not valid_message:
-        raise InputError("Error: message_id does not refer to a \
-                         valid message within the current channel")
+    
+    for dm in db_store['dms']:
+        for message in dm['messages']:
+            if message['message_id'] == message_id:
+                valid_dm = True
+                dm_id = message['dm_id']
+                dm_owner = dm_details_v1(auth_user_id, dm_id)['members'][0]
+                if message['u_id'] == auth_user_id:
+                    auth_request = True
+                elif dm_owner['u_id'] == auth_user_id:
+                    auth_request = True       
+                if valid_dm == True and auth_request == False:
+                    raise AccessError("Authorised user does not have owner permisson \
+                          of the channel or the message was not sent by the \
+                          authorised user making this request")
+            if not valid_dm:
+                raise InputError("Error: message_id does not refer to a \
+                         valid message within the current dm")
+                
+    if valid_dm and auth_request:
+        for dm in db_store['dms']:
+            for message in dm['messages']:
+                if message['message_id'] == message_id:
+                    dm['messages'] = new_message
+                    save_database_updates(db_store)
 
+    if valid_dm == False and valid_channel_message == False:
+        raise InputError("Error: message_id oes not refer to a valid message within \
+                         a channel /DM that the authorised user has joined")   
 
 def message_remove_v1(token, message_id):
     '''
@@ -173,8 +193,8 @@ def message_remove_v1(token, message_id):
     # Fetch data
     auth_request = False
     has_owner_permission = False
-    valid_message = False
-
+    valid_channel_message = False
+    valid_dm = False
 
     db_store = get_data()
         
@@ -187,12 +207,13 @@ def message_remove_v1(token, message_id):
         if user['u_id'] == auth_user_id:
             targer_user = user
             
+    #Remove channel messages       
     for channel in db_store['channels']:
         for message in channel['messages']:
             if message['message_id'] == message_id:
-                valid_message = True
+                valid_channel_message = True
                 if message['u_id'] == auth_user_id:
-                    auth_request = True
+                    auth_request = True        
                 for users in channel['owner_members']:
                     if auth_user_id == users['u_id']:
                         has_owner_permission = True
@@ -202,16 +223,41 @@ def message_remove_v1(token, message_id):
                     raise AccessError("Authorised user does not have owner permisson \
                           of the channel or the message was not sent by the \
                           authorised user making this request")
-    if valid_message and auth_request:
+                
+    if valid_channel_message and auth_request:
         for channel in db_store['channels']:
             for message in channel['messages']:
                 if message['message_id'] == message_id:
                     channel['messages'].remove(message)
                     save_database_updates(db_store)
-                    
-    if not valid_message:
-        raise InputError("Error: message_id does not refer to a \
-                         valid message within the current channel")
+        
+    #Remove dm messages
+    for dm in db_store['dms']:
+        for message in dm['messages']:
+            if message['message_id'] == message_id:
+                dm_id = message['dm_id']
+                dm_owner = dm_details_v1(auth_user_id, dm_id)['members'][0]
+                valid_dm = True
+                if message['u_id'] == auth_user_id:
+                    auth_request = True
+                elif dm_owner['u_id'] == auth_user_id:
+                    auth_request = True
+                if valid_dm == True and auth_request == False:
+                    raise AccessError("Authorised user does not have owner permisson \
+                          of the channel or the message was not sent by the \
+                          authorised user making this request")
+                
+    if valid_dm and auth_request:
+        for dm in db_store['dms']:
+            for message in dm['messages']:
+                if message['message_id'] == message_id:
+                    dm['messages'].remove(message)
+                    save_database_updates(db_store)               
+
+    if valid_dm == False and valid_channel_message == False:
+        raise InputError("Error: message_id oes not refer to a valid message within \
+                         a channel /DM that the authorised user has joined")
+                  
     
 def message_senddm_v1(token, dm_id, message):
     '''
@@ -229,29 +275,33 @@ def message_senddm_v1(token, dm_id, message):
     '''
     # Fetch data
     is_member = False
+    valid_dm = False
     if(len(message)<1 or len(message)>1000):
         raise InputError("Error: message too long or too short")
 
     db_store = get_data()
     #Get authorised user id 
-    u_id = decode_jwt(token)['u_id']
+    auth_user_id = decode_jwt(token)['u_id']
+    for user in db_store['users']:
+        if user['u_id'] == auth_user_id:
+            targer_user = user    
 
     #Check if dm_id is not valid
     for dm in db_store['dms']:
         if dm['dm_id'] == dm_id:
             target_dm = dm
-        else:
-            raise InputError(description="dm_id does not refer to a valid dm id")
+            valid_dm = True
+            if dm['auth_user_id'] == auth_user_id:
+                is_member = True # Check if authorised user is a member of the dm
+            for members in dm_details_v1(auth_user_id, dm_id)['members']:
+                if targer_user['u_id'] == members['u_id']:
+                    is_member = True # Check if authorised user is a member of the dm
 
-    for dm in db_store['dms']:
-        if dm['auth_user_id'] == u_id:
-            is_member = True# Check if authorised user is a member of the channel
-            break
+    if is_member == False and valid_dm == True:
+        raise AccessError(description="Authorised user is not a member of DM")
+    if valid_dm == False:
+        raise InputError(description="dm_id does not refer to a valid dm id")
    
-    if is_member == False:
-        raise AccessError("Authorised user is not a member of DM")
-   
-    #seek_target_channel_and_errors(db_store, u_id, channel_id)
     message_id = db_store['message_index']
     db_store['message_index']+=1
 
@@ -260,8 +310,9 @@ def message_senddm_v1(token, dm_id, message):
     
     dm_message = {
         'message_id': message_id,
-        'u_id': u_id,
+        'u_id': auth_user_id,
         'message': message,
+        'dm_id':dm_id,
         'time_created': timestamp
     }
 
--- tests/message_remove_http_test.py
+++ tests/message_remove_http_test.py
@@ -1,7 +1,6 @@
 import requests
 import pytest
 import json
-
 from src import config
 
 BASE_URL = config.url
@@ -34,8 +33,6 @@ def test_cannot_remove_deleted_message_http():
 
     msg_id_1 = requests.post(f"{BASE_URL}/message/send/v1",json = message_send_program_1).json()
 
-
-    
     message_remove_program_1 = {
         'token': auth_user_1['token'],
         'message_id': msg_id_1['message_id']
@@ -47,7 +44,7 @@ def test_cannot_remove_deleted_message_http():
     assert dm_remove_2.status_code == 400      
 
 
-def test_msg_rm_invalid_msg_id():
+def test_msg_remove_invalid_msg_id_1_http():
     requests.delete(f"{BASE_URL}/clear/v1", json = {})
     
     message_1 = "dsdaada"
@@ -114,7 +111,7 @@ def test_msg_rm_invalid_msg_id():
     dm_remove_2 = requests.delete(f"{BASE_URL}/message/remove/v1", json = message_remove_program_2)
     assert dm_remove_2.status_code == 403 
 
-def test_msg_rm_invalid_msg_id_2():
+def test_msg_remove_invalid_msg_id_2_http():
     requests.delete(f"{BASE_URL}/clear/v1", json = {})
     
     message_1 = "dsdaada"
@@ -152,7 +149,7 @@ def test_msg_rm_invalid_msg_id_2():
     dm_remove_1 = requests.delete(f"{BASE_URL}/message/remove/v1", json = message_remove_program_1)
     assert dm_remove_1.status_code == 400  
 
-def test_global_owner_can_remove_members_message_channel():
+def test_global_owner_can_remove_members_message_channel_http():
     requests.delete(f"{BASE_URL}/clear/v1", json = {})
     
     register_param_1 = {
@@ -169,7 +166,6 @@ def test_global_owner_can_remove_members_message_channel():
         'is_public': True
     }
     channel = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
-
 
     register_param_2 = {
         "email": "test2@gmail.com",
@@ -206,4 +202,383 @@ def test_global_owner_can_remove_members_message_channel():
 
     response = requests.delete(f"{BASE_URL}/message/remove/v1",json = message_remove_program)
 
+    assert response.status_code == 200
+
+def test_owner_can_remove_members_message_channel_http():
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json()
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    register_param_3 = {
+        "email": "test3@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Johnson"
+    }
+    user_3 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_3).json()    
+
+    channel_param = {
+        'token': user_1['token'],
+        'name': 'league',
+        'is_public': True
+    }
+    channel = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
+    '''
+    channel_invites_param_1 = {
+        'token': user_1['token'],
+        'channel_id': channel['channel_id'],
+        'u_id': user_2['auth_user_id']
+    }
+    requests.post(f"{BASE_URL}/channel/invite/v2", json = channel_invites_param_1)
+    
+    channel_invites_param_2 = {
+        'token': user_1['token'],
+        'channel_id': channel['channel_id'],
+        'u_id': user_3['auth_user_id']
+    }
+    
+    requests.post(f"{BASE_URL}/channel/invite/v2", json = channel_invites_param_2)
+    '''
+    channel_join_param = {
+        'token': user_2['token'],
+        'channel_id': channel['channel_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/join/v2", json = channel_join_param)
+
+    channel_join_param = {
+        'token': user_3['token'],
+        'channel_id': channel['channel_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/join/v2", json = channel_join_param)
+    
+    channel_addowner_param = {
+        'token': user_1['token'],
+        'channel_id': channel['channel_id'],
+        'u_id': user_2['auth_user_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/addowner/v1", json = channel_addowner_param)
+    
+    msg = "test"
+    message_send_program = {
+        'token': user_3['token'],
+        'channel_id': channel['channel_id'],
+        'message': msg
+    }
+    
+    target_message = requests.post(f"{BASE_URL}/message/send/v1",json = message_send_program)
+    assert target_message.status_code == 200
+    target_message = target_message.json()
+    '''
+    ch_join_param = {
+        "token": user_1["token"], 
+        "channel_id": channel["channel_id"]
+    }
+    get_request = requests.get(f"{BASE_URL}/channel/details/v2", params = ch_join_param)
+    assert get_request.status_code == 200
+    request_data = get_request.json()
+    print(request_data["owner_members"])
+    '''
+    message_remove_program = {
+        'token': user_2['token'],
+        'message_id': target_message['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = message_remove_program)
+
     assert response.status_code == 200    
+
+def test_original_poster_can_remove_message_channel_http():    
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json()
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    channel_param = {
+        'token': user_1['token'],
+        'name': 'league',
+        'is_public': True
+    }
+    channel = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
+
+    channel_join_param = {
+        'token': user_2['token'],
+        'channel_id': channel['channel_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/join/v2", json = channel_join_param)
+    
+    msg = "test"
+    message_send_program = {
+        'token': user_2['token'],
+        'channel_id': channel['channel_id'],
+        'message': msg
+    }
+    
+    target_message = requests.post(f"{BASE_URL}/message/send/v1",json = message_send_program).json()
+
+    message_remove_program = {
+        'token': user_2['token'],
+        'message_id': target_message['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = message_remove_program)
+
+    assert response.status_code == 200
+
+def test_nonowner_nonposter_cant_remove_http():
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json()
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    register_param_3 = {
+        "email": "test4@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Johnson"
+    }
+    user_3 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_3).json()
+
+    channel_param = {
+        'token': user_1['token'],
+        'name': 'league',
+        'is_public': True
+    }
+    channel = requests.post(f"{BASE_URL}/channels/create/v2", json = channel_param).json()
+
+    channel_join_param_1 = {
+        'token': user_2['token'],
+        'channel_id': channel['channel_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/join/v2", json = channel_join_param_1)
+
+    channel_join_param_2 = {
+        'token': user_3['token'],
+        'channel_id': channel['channel_id']
+    }
+    response = requests.post(f"{BASE_URL}/channel/join/v2", json = channel_join_param_2)
+    
+    msg = "test"
+    message_send_program = {
+        'token': user_2['token'],
+        'channel_id': channel['channel_id'],
+        'message': msg
+    }
+    
+    target_message = requests.post(f"{BASE_URL}/message/send/v1",json = message_send_program).json()
+
+    message_remove_program = {
+        'token': user_3['token'],
+        'message_id': target_message['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = message_remove_program)
+
+    assert response.status_code == 403
+
+def test_original_poster_can_remove_message_dm_http():
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json()
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    register_param_3 = {
+        "email": "test4@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Johnson"
+    }
+    user_3 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_3).json()
+
+    u_id1 = user_1['auth_user_id']
+    u_id2 = user_2['auth_user_id']
+    u_ids = [u_id1,u_id2]
+
+    dm_param = {
+        'token': user_3['token'],
+        'u_ids': u_ids
+    }
+    dm = requests.post(f"{BASE_URL}/dm/create/v1", json = dm_param).json()
+    
+    msg = "test"
+    
+    dm_send_program = {
+        'token': user_3['token'],
+        'dm_id': dm['dm_id'],
+        'message': msg
+    }
+    
+    target_dm = requests.post(f"{BASE_URL}/message/senddm/v1",json = dm_send_program).json()
+
+    dm_remove_program = {
+        'token': user_3['token'],
+        'message_id': target_dm['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = dm_remove_program)
+
+    assert response.status_code == 200
+
+def test_global_owner_cant_remove_members_message_dm_http():
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json() #permission_id = 1
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    register_param_3 = {
+        "email": "test4@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Johnson"
+    }
+    user_3 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_3).json()
+
+    u_id1 = user_1['auth_user_id']
+    u_id2 = user_2['auth_user_id']
+    u_ids = [u_id1,u_id2]
+
+    dm_param = {
+        'token': user_3['token'],
+        'u_ids': u_ids
+    }
+    dm = requests.post(f"{BASE_URL}/dm/create/v1", json = dm_param).json()
+    
+    msg = "test"
+    
+    dm_send_program = {
+        'token': user_3['token'],
+        'dm_id': dm['dm_id'],
+        'message': msg
+    }
+    
+    target_dm = requests.post(f"{BASE_URL}/message/senddm/v1",json = dm_send_program).json()
+
+    dm_remove_program = {
+        'token': user_1['token'],
+        'message_id': target_dm['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = dm_remove_program)
+
+    assert response.status_code == 403
+
+def test_owner_can_remove_members_message_dm_http():
+    requests.delete(f"{BASE_URL}/clear/v1", json = {})
+    
+    register_param_1 = {
+        "email": "test1@gmail.com",
+        "password": "abcd1234",
+        "name_first": "John",
+        "name_last": "Smith"
+    }
+    user_1 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_1).json() #permission_id = 1
+
+    register_param_2 = {
+        "email": "test2@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Smith"
+    }
+    user_2 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_2).json()
+
+    register_param_3 = {
+        "email": "test4@gmail.com",
+        "password": "abcd1234",
+        "name_first": "Agent",
+        "name_last": "Johnson"
+    }
+    user_3 = requests.post(f"{BASE_URL}/auth/register/v2", json = register_param_3).json()
+
+    u_id1 = user_1['auth_user_id']
+    u_id2 = user_2['auth_user_id']
+    u_ids = [u_id1,u_id2]
+
+    dm_param = {
+        'token': user_3['token'],
+        'u_ids': u_ids
+    }
+    dm = requests.post(f"{BASE_URL}/dm/create/v1", json = dm_param).json()
+    
+    msg = "test"
+    
+    dm_send_program = {
+        'token': user_2['token'],
+        'dm_id': dm['dm_id'],
+        'message': msg
+    }
+    
+    target_dm = requests.post(f"{BASE_URL}/message/senddm/v1",json = dm_send_program).json()
+
+    dm_remove_program = {
+        'token': user_3['token'],
+        'message_id': target_dm['message_id'],
+    }
+
+    response = requests.delete(f"{BASE_URL}/message/remove/v1",json = dm_remove_program)
+
+    assert response.status_code == 200
