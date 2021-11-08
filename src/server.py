021aa0e87182207ad3b7a65a81ff98ee1a499375
Weizhe Diao <z5338676@ad.unsw.edu.au>
2021-11-09 10:08:06 +1100

    user/stats/v1

--- src/server.py
+++ src/server.py
@@ -11,7 +11,7 @@ from src import message
 from src.channel import channel_addowner_v1, channel_invite_v1, channel_join_v1, channel_details_v1, channel_removeowner_v1, channel_messages_v1
 from src.channel import channel_leave_v1
 from src.user import user_profile_v1, user_profile_setname_v1, user_profile_setemail_v1
-from src.user import user_profile_sethandle_v1, users_all_v1, user_profile_uploadphoto_v1
+from src.user import user_profile_sethandle_v1, users_all_v1, user_profile_uploadphoto_v1, user_stats
 from src.users import users_all_v1
 from src.channels import channels_create_v1
 from src.dm import dm_create_v1, dm_details_v1, dm_messages_v1, dm_list_v1, dm_remove_v1
@@ -644,6 +644,12 @@ def list_users():
     
     users = users_all_v1()
     return dumps(users)
+
+@APP.route('/user/stats/v1', methods=['GET'])
+def user_stats():
+    token = request.args.get('token')
+    stats = user_stats_v1(token)
+    return dumps(stats)
 
 @APP.route("/admin/user/remove/v1", methods=['DELETE'])
 def remove_user():
